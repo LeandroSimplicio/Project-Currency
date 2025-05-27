f5691623154642869f8d3c68f50552d8c06afcb1
Leandro Simplicio <nightmare.maggot@gmail.com>
2025-05-27 09:49:47 -0300

    feat: feito a estilização do css

---
+++ Assets/script.js
@@ -0,0 +1,9 @@
+const convertButton = document.querySelector("btn-convert");
+
+function converValues() {
+//   const inputCurrencyValue = document.querySelector("input-currency").value;
+
+  console.log("Funciou");
+}
+
+// convertButton.addEventListener("click", converValues);
