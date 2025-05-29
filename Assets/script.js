f365af029104cba04e1f546320509ff74d0cc601
Leandro Simplicio <nightmare.maggot@gmail.com>
2025-05-29 17:08:36 -0300

    feat: mapeando as variaveis no javascript

--- Assets/script.js
+++ Assets/script.js
@@ -4,6 +4,30 @@ const convertButton = document.querySelector(".btn-convert");
 
 function convertValues() {
   const inputCurrencyValue = document.querySelector(".input-currency").value;
+
+  const currencyValueToConvert = document.querySelector(".currency-value-to-convert"); //Pega o valor a ser convertido
+  const currencyValueToConverted = document.querySelector(".currency-value"); // pega o valor convertido
+
+  
+  
+  const dolarToDay = 5.2
+  cosnt euroToDay = 6.2 // Valores fictícios para a cotação do dólar e euro, você pode substituir por uma API real
+  // ;
+
+  const convertedValue = inputCurrencyValue / dolarToDay;
+
+  currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL"
+  }).format(inputCurrencyValue);
+  // Formata o valor a ser convertido para o formato de moeda brasileiro
+
+  currencyValueToConverted.innerHTML = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD"
+  }).format(convertedValue);
+  // Formata o valor convertido para o formato de moeda americana
+
   
 
 }
