c6cbf205427b8988bb75e82354e204d683e01735
Leandro Simplicio <nightmare.maggot@gmail.com>
2025-06-03 14:02:14 -0300

    feat: melhorias

--- Assets/script.js
+++ Assets/script.js
@@ -1,10 +1,11 @@
-const convertButton = document.querySelector("btn-convert");
+const convertButton = document.querySelector('.btn-convert');
 
-function converValues() {
-  const inputCurrencyValue = document.querySelector("input-currency").value;
+function convertCurrency() {
+    const inputCurrency = document.querySelector('.input-currency').value;
 
+    const dolartoDay = 5.25; // Example exchange rate for USD to BRL
+
+    const convertedValue =  inputCurrency / dolartoDay;
 }
 
-convertButton.addEventListener("click", converValues);
-
-console.log("Funciou");
+convertButton.addEventListener("click", convertCurrency);
